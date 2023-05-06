f306236612dc18615bc82d2fef61b538930eee06
khaderman2000 <1181139@student.birzeit.edu>
2023-05-06 19:18:25 +0300

    v6

--- app/src/main/java/com/example/assignment/Converter.java
+++ app/src/main/java/com/example/assignment/Converter.java
@@ -3,10 +3,14 @@ package com.example.assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Converter extends AppCompatActivity {
@@ -19,6 +23,34 @@ public class Converter extends AppCompatActivity {
         Button calculateBtn = findViewById(R.id.calculateBtn);
         EditText fromEditTxt = findViewById(R.id.fromEditTxt);
         TextView toTxtVeiw = findViewById(R.id.toTxtVeiw);
+        Spinner unitSpinner = findViewById(R.id.unitsSpinner);
+        final int[] unit = {0};
+
+        unitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                if (item != null && item.equals("Length")) {
+                    fromEditTxt.setHint("in meters");
+                    fromEditTxt.setText("");
+                    toTxtVeiw.setText("The result");
+                    unit[0] =0;
+                } else {
+                    fromEditTxt.setHint("in kg");
+                    fromEditTxt.setText("");
+                    toTxtVeiw.setText("The result");
+                    unit[0] =1;
+                }
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+
+
+        });
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,9 +61,35 @@ public class Converter extends AppCompatActivity {
         calculateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                float number = Float.parseFloat((fromEditTxt.getText().toString()));
-                number *=100;
-                toTxtVeiw.setText(Float.toString(number)+"cm");
+                if ((fromEditTxt.getText().toString())!=null&&
+                        TextUtils.isDigitsOnly(fromEditTxt.getText().toString())) {
+                    if (unit[0] == 0) {
+                        float number = Float.parseFloat((fromEditTxt.getText().toString()));
+                        number *= 0.000621371192;
+                        toTxtVeiw.setText(Float.toString(number) + "miles");
+                    } else {
+                        float number = Float.parseFloat((fromEditTxt.getText().toString()));
+                        number *= 2.20462262;
+                        toTxtVeiw.setText(Float.toString(number) + "pounds");
+                    }
+                    toTxtVeiw.setTextColor(Color.BLACK);
+                }
+                else {
+
+                    if(unit[0]==0){
+                        fromEditTxt.setHint("in meters");
+                        fromEditTxt.setText("");
+                        toTxtVeiw.setText("The result");
+
+                    } else {
+                        fromEditTxt.setHint("in kg");
+                        fromEditTxt.setText("");
+                        toTxtVeiw.setText("The result");
+
+                    }
+
+
+                }
             }
         });
     }
